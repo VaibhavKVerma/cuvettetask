1cc5c7e7382ab0679bf03c7d1f154505e2c8a76e
vaibhav <vaibhavverma15062001@gmail.com>
2022-06-05 12:41:53 +0530

    graph changes

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,8 +1,13 @@
 import { Line } from "react-chartjs-2";
+import annotationPlugin from "chartjs-plugin-annotation";
+import { useContext } from "react";
+import { Context } from "../Context";
 
 const Graph = () => {
-  const data = {
-    labels: [0, 20, 40, 60, 80, 100],
+  const [data] = useContext(Context);
+
+  const Data = {
+    labels: ["0%", "20%", "40%", "60%", "80%", "100"],
     datasets: [
       {
         data: [65, 59, 70, 76, 66, 55],
@@ -18,12 +23,50 @@ const Graph = () => {
       legend: {
         display: false,
       },
+      annotation: {
+        annotations: {
+          box1: {
+            type: "box",
+            xMin: Math.max(0, (data.percentile / 100) * 5 - 0.5),
+            xMax: Math.min(5, (data.percentile / 100) * 5 + 0.5),
+            yMin: 0,
+            yMax: 100,
+            backgroundColor: "rgba(99, 211, 255, 0.25)",
+            borderWidth: 0,
+          },
+          line1: {
+            type: "line",
+            xMax: (data.percentile / 100) * 5,
+            xMin: (data.percentile / 100) * 5,
+            borderColor: "#438af6 ",
+            borderWidth: 2,
+            borderDash: [6, 4],
+          },
+          line2: {
+            type: "line",
+            xMax: (72 / 100) * 5,
+            xMin: (72 / 100) * 5,
+            borderColor: "#ff9142  ",
+            borderWidth: 2,
+            borderDash: [6, 4],
+          },
+        },
+      },
+    },
+    elements: {
+      point: {
+        radius: 0,
+      },
+    },
+    scales: {
+      y: { ticks: { display: false }, grid: { color: "white" } },
+      x: { grid: { color: "white" } },
     },
   };
 
   return (
     <>
-      <Line data={data} options={options} />
+      <Line data={Data} options={options} plugins={[annotationPlugin]} />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Chart } from 'chart.js';
+import annotationPlugin from 'chartjs-plugin-annotation';
+
+Chart.register(annotationPlugin);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
