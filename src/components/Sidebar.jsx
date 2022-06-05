afd78bbf0ebc67c3649f88a4a3d9cc4b0d73cb9e
vaibhav <vaibhavverma15062001@gmail.com>
2022-06-05 12:51:09 +0530

    minor changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -26,19 +26,7 @@ const Sidebar = () => {
             </div>
             <div className="DashboardPanel_navigation__10Cmg DashboardPanel_active__jHRLB">
               <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
-                </svg>
+                <img src="/images/skill.png" alt="skill" width={"17px"} />
               </div>
               Skill Test
             </div>
